6f8a6e3dc0711f7db9007dba375b62d5f560fb8a
Sebastián Burgos <snburgos@uc.cl>
2023-06-11 20:36:46 -0400

    Added AQI explain table

--- meteo-09/src/components/airPollution/AirPollutionItem.svelte
+++ meteo-09/src/components/airPollution/AirPollutionItem.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import Modal from '../Modal.svelte';
   import type { AirPollution } from '../../scripts/airPollution';
 
   export let airPollution: AirPollution;
+
+  let showModal: boolean = false;
 
   const aqiColors = {
     1: 'is-success',
@@ -15,10 +18,13 @@
 <div class="card my-2">
   <div class="card-content">
     <div class="content">
-      <span class={`tag is-medium ${aqiColors[airPollution.aqi]}`}>
-        AQI (Índice de Calidad del Aire):
-        {airPollution.aqi}
-      </span>
+      <button
+        class={`tag is-medium ${aqiColors[airPollution.aqi]}`}
+        on:click={() => (showModal = true)}
+      >
+        AQI (Índice de Calidad del Aire):&nbsp;
+        <b>{airPollution.aqi}</b>
+      </button>
     </div>
     <div class="content">
       <p class="title is-4">{airPollution.dt}</p>
@@ -51,3 +57,78 @@
     </div>
   </div>
 </div>
+
+<Modal bind:showModal>
+  <h2 slot="header">Explicación AQI (Índice de Calidad del Aire)</h2>
+  <table class="table is-bordered is-striped is-hoverable">
+    <thead>
+      <tr>
+        <th>Nombre cualitativo</th>
+        <th>AQI</th>
+        <th colspan="6">Concentración de contaminantes en μg/m<sup>3</sup></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td />
+        <td />
+        <td>SO<sub>2</sub></td>
+        <td>NO<sub>2</sub></td>
+        <td>PM<sub>10</sub></td>
+        <td>PM<sub>2.5</sub></td>
+        <td>O<sub>3</sub></td>
+        <td>CO</td>
+      </tr>
+      <tr>
+        <td>Buena</td>
+        <td class={`tag is-medium ${aqiColors[1]}`}>1</td>
+        <td>[0; 20)</td>
+        <td>[0; 40)</td>
+        <td>[0; 20)</td>
+        <td>[0; 10)</td>
+        <td>[0; 60)</td>
+        <td>[0; 4400)</td>
+      </tr>
+      <tr>
+        <td>Justa</td>
+        <td class={`tag is-medium ${aqiColors[2]}`}>2</td>
+        <td>[20; 80)</td>
+        <td>[40; 70)</td>
+        <td>[20; 50)</td>
+        <td>[10; 25)</td>
+        <td>[60; 100)</td>
+        <td>[4400; 9400)</td>
+      </tr>
+      <tr>
+        <td>Moderada</td>
+        <td class={`tag is-medium ${aqiColors[3]}`}>3</td>
+        <td>[80; 250)</td>
+        <td>[70; 150)</td>
+        <td>[50; 100)</td>
+        <td>[25; 50)</td>
+        <td>[100; 140)</td>
+        <td>[9400-12400)</td>
+      </tr>
+      <tr>
+        <td>Pobre</td>
+        <td class={`tag is-medium ${aqiColors[4]}`}>4</td>
+        <td>[250; 350)</td>
+        <td>[150; 200)</td>
+        <td>[100; 200)</td>
+        <td>[50; 75)</td>
+        <td>[140; 180)</td>
+        <td>[12400; 15400)</td>
+      </tr>
+      <tr>
+        <td>Muy pobre</td>
+        <td class={`tag is-medium ${aqiColors[5]}`}>5</td>
+        <td>⩾350</td>
+        <td>⩾200</td>
+        <td>⩾200</td>
+        <td>⩾75</td>
+        <td>⩾180</td>
+        <td>⩾15400</td>
+      </tr>
+    </tbody>
+  </table>
+</Modal>
